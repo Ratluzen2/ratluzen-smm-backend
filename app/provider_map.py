d8af0b5d4f8f0281ea6ceaed0ba6cef2909d2d09
Ratluzen Services <zafer.s.2.693@gmail.com>
2025-10-09 02:23:44 +0300

    Delete app/provider_map.py

--- app/provider_map.py
+++
@@ -1,8 +0,0 @@
-from .config import settings
-
-def get_provider():
-    return {
-        "name": "default",
-        "api_url": settings.SMM_API_URL.rstrip("/"),
-        "api_key": settings.SMM_API_KEY,
-    }
