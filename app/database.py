3ef05bcd21fffbd9c281e3684dc919e7b77df882
Ratluzen Services <zafer.s.2.693@gmail.com>
2025-10-08 08:34:25 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,12 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
-# Heroku's DATABASE_URL may start with 'postgres://' which SQLAlchemy dislikes; fix to 'postgresql://'
 db_url = settings.DATABASE_URL.replace("postgres://", "postgresql://")
-
 engine = create_engine(db_url, pool_pre_ping=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
-
 Base = declarative_base()
 
 def get_db():
