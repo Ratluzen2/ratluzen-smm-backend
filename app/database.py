5585c698e31c0784f2841ee292e0ed131bf75098
Ratluzen Services <zafer.s.2.693@gmail.com>
2025-10-08 16:24:33 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,8 +3,13 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
